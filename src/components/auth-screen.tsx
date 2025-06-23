c63bd1f076ef7bd0d2d793f7804c7718364934cb
LeonardoNXT <leonardoffice12@gmail.com>
2025-06-22 22:19:51 -0300

    FIX: ERRO TO LOADPAGE

--- src/components/auth-screen.tsx
+++ src/components/auth-screen.tsx
@@ -40,12 +40,15 @@ export default function AuthScreen() {
       button.current.textContent = "Entrando...";
       button.current.disabled = true;
 
-      const response = await fetch("http://127.0.0.1:3005/auth/login", {
-        method: "POST",
-        credentials: "include",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(user),
-      });
+      const response = await fetch(
+        "backend-feelflow-core.onrender.com/auth/login",
+        {
+          method: "POST",
+          credentials: "include",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(user),
+        }
+      );
 
       if (!response.ok) {
         throw new Error(await response.text());
