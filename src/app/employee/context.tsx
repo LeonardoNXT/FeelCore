82416b311f6aaa6474c24c9eb67836eb545bd0af
LeonardoNXT <leonardoffice12@gmail.com>
2025-08-28 09:14:59 -0300

    feat(Employee Home): Add part to scheduling

--- src/app/employee/context.tsx
+++ src/app/employee/context.tsx
@@ -8,6 +8,7 @@ import {
   Cog,
   BookOpenCheck,
   Notebook,
+  MoveUpRight
 } from "lucide-react";
 
 export default function MainRouteOfEmployeeConteiner() {
@@ -38,13 +39,20 @@ export default function MainRouteOfEmployeeConteiner() {
 
   return (
     <section className="w-full min-h-screen relative bg-[#000] bg-[url('/background.jpg')] bg-cover bg-center">
-      <div className="w-full min-h-full absolute top-0 backdrop-blur-2xl backdrop-sepia-90">
-        <div className="px-5 py-40">
+      <div className="w-full min-h-screen top-0 backdrop-blur-2xl backdrop-sepia-90">
+        <div className="px-5 pt-40">
           <p className="text-5xl">Bem-vindo, é bom te ver aqui!</p>
           <p className="text-2xl text-[#fff8e2]">
             {dia}.{mes} - {diaAtual}
           </p>
         </div>
+    <div className="px-4 mt-10">
+      <div className="w-full px-2 py-2 bg-amber-200 relative rounded-[40px] text-[#333] flex justify-end">
+        <div className="p-4 bg-[#000] w-max rounded-full">
+          <MoveUpRight className="text-[#ebebeb]" />
+        </div>
+      </div>
+    </div>
       </div>
     </section>
   );
